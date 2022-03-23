f38dbfffd2bc9a56c57685403df683b8efe05207
Bastien Arlot <arlot.b@gmail.com>
2022-03-23 17:07:25 +0100

    add firstname and lastname to sign up with validation alphanumeric and edited the basic view, add of the home controller with a verifcation of the state of the user

--- db/migrate/20220323144834_devise_create_users.rb
+++ db/migrate/20220323144834_devise_create_users.rb
@@ -6,6 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :firstname,          null: false, default: ""
+      t.string :lastname,           null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
