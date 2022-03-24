e33712fd3e7767a84998607b22659bf97e6498a6
alegarn <alexgarnier78310@protonmail.com>
2022-03-24 18:23:39 +0100

    Answer team controller, new.html.erb

---
+++ app/helpers/answer_teams_helper.rb
@@ -0,0 +1,2 @@
+module AnswerTeamsHelper
+end
---
+++ test/controllers/answer_teams_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AnswerTeamsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
