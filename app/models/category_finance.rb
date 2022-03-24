54e29c0c4855b51947c8d3ab14016e1a34cd9f4f
guillaume <reygner0224@gmail.com>
2022-03-24 11:48:23 +0100

    add answer finance model with category finance association and answer finance table migration

--- app/models/category_finance.rb
+++ app/models/category_finance.rb
@@ -1,3 +1,4 @@
 class CategoryFinance < ApplicationRecord
   belongs_to :score
+  has_one :answer_finance, dependent: :destroy
 end
---
+++ test/models/answer_finance_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AnswerFinanceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
