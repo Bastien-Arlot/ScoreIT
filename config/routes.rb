52f040e62fbcd1f8b0dfa8c9ca43eead43b9d383
Pierre SARAZIN <pierres@macbook-pro-de-pierre.home>
2022-03-25 15:32:17 +0100

    creation of the team page in html format, creation of the css page associated to the team page, addition of a route to access the home/team page, addition of arnaud's picture, addition of the github logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/private'
+  get 'home/team'
+
   devise_for :users, controllers: {
     omniauth_callbacks:  'users/omniauth_callbacks'
   }
