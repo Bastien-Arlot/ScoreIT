8422e2d9c7cb5ff2937816d694bfcff71ade4da0
alegarn <alexgarnier78310@protonmail.com>
2022-03-25 18:05:08 +0100

    error fixed: change association innovation model / validates with innovation/... models

--- app/models/category_strategy.rb
+++ app/models/category_strategy.rb
@@ -2,6 +2,8 @@ class CategoryStrategy < ApplicationRecord
   belongs_to :score
   has_one :answer_strategy, dependent: :destroy
 
+  validates :score_id, uniqueness: true
+
   after_update :scoring_total
 
 end
