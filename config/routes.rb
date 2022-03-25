a93e282f9b608ffd56e620f3840c589ccd69c546
alegarn <alexgarnier78310@protonmail.com>
2022-03-25 10:23:11 +0100

    routes modified, categories already tested, answers model tested team and market

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,25 @@ Rails.application.routes.draw do
     omniauth_callbacks:  'users/omniauth_callbacks'
   }
 
-  resources :startups, only: [:index, :new, :create, :update]
-  
-  resources :scores, only: [:index, :new, :create, :update] do
-    resources :category_markets, except: [:index, :new, :create, :update, :destroy, :show] do
-          resources :answer_markets, only: [:new, :create]
+  resources :startups, only: [:index, :new, :create, :update] do
+
+    resources :scores, only: [:index, :new, :create, :update] do
+
+      resources :category_markets, except: [:index, :new, :create, :update, :destroy, :show] do
+        resources :answer_markets, only: [:new, :create]
+      end
+
+      resources :category_teams, except: [:index, :new, :create, :show, :update] do
+        resources :answer_teams, only: [:new, :create]
+      end
+
+      resources :category_innovations, except: [:index, :new, :create, :show, :update] do
+        resources :answer_innovations, only: [:new, :create]
+      end
+
     end
-    resources :category_teams, except: [:index, :new, :create, :show, :update] do
-      resources :answer_teams, only: [:new, :create]
-    end
+    
   end
+
+
 end
