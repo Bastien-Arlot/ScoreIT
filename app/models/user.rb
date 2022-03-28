60500f553aa9c114ab9a8e0d375228ce47fb41c1
Bastien Arlot <arlot.b@gmail.com>
2022-03-28 12:59:21 +0200

    authentification with github, definition of the scope to get private email, modification of the controller and initialization of the methods to retrieve content

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,12 @@ class User < ApplicationRecord
     end
   end
 
+  def self.from_github(auth)
+    where(github_id: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.username = auth.info.name
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
 end
