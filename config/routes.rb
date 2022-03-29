37fb693d53cf900f300b877f47e3ca7106c4c5c7
Pierre SARAZIN <pierres@macbook-pro-de-pierre.home>
2022-03-29 18:33:18 +0200

    fixed an error (an end in config/routes.rb was removed during conflict resolution in the last pull request) so it was added here

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,8 @@ Rails.application.routes.draw do
   }
   
   scope 'admin', module: 'admin', as: 'admin' do
-    
   resources :users
-    
+  end
 
   resources :user, only: [:show, :edit, :update]
 
