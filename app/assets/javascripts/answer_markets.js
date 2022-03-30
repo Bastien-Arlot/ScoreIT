a3be61e6c3f8f87894c19e700acf151407fc7cde
guillaume <reygner0224@gmail.com>
2022-03-30 23:42:10 +0200

    test send to heroku

--- app/assets/javascripts/answer_markets.js
+++ app/assets/javascripts/answer_markets.js
@@ -1,49 +1,49 @@
-// window.addEventListener("load", function(){
+window.addEventListener("load", function(){
 
-//   const notyf = new Notyf({
-//     duration: 6000,
-//     position: {
-//       x: 'right',
-//       y: 'top',
-//     }
-//   });
+  const notyf = new Notyf({
+    duration: 6000,
+    position: {
+      x: 'right',
+      y: 'top',
+    }
+  });
 
-//   let answer_market_4 = document.getElementById("answer_market_4");
-//   let answer_market_3 = document.getElementById("answer_market_3");
+  let answer_market_4 = document.getElementById("answer_market_4");
+  let answer_market_3 = document.getElementById("answer_market_3");
 
-//   window.addEventListener("load", function() {
-//       answer_market_3.value = "50";
-//       answer_market_4.value = "50";
-//     });
+  window.addEventListener("load", function() {
+      answer_market_3.value = "50";
+      answer_market_4.value = "50";
+    });
 
-//     answer_market_3.addEventListener("change", function() {
-//     if (answer_market_3.value == "100"){
-//         document.getElementById("sizemarket").textContent = "+ 100";
-//     }else{
-//         document.getElementById("sizemarket").textContent = answer_market_3.value;
-//     }
-//   }, false);
+    answer_market_3.addEventListener("change", function() {
+    if (answer_market_3.value == "100"){
+        document.getElementById("sizemarket").textContent = "+ 100";
+    }else{
+        document.getElementById("sizemarket").textContent = answer_market_3.value;
+    }
+  }, false);
 
-//   answer_market_4.addEventListener("change", function() {
-//       document.getElementById("target").textContent = answer_market_4.value;
-//   }, false);
+  answer_market_4.addEventListener("change", function() {
+      document.getElementById("target").textContent = answer_market_4.value;
+  }, false);
 
 
-//   let error_market = document.getElementById("error_message_market");
-//   let answer_market = [];
+  let error_market = document.getElementById("error_message_market");
+  let answer_market = [];
 
-//   if (error_market.textContent !== []){
-//     answer_market = error_market.textContent.split(',');
-//     answer_market.forEach(element => {
-//       for (let i = 1; i < 9; i++){
-//         if (element.includes(`market ${i}`)){
-//               notyf.error({
-//                   message: `Merci de répondre à la question n°${i}`,
-//                   dismissible: true
-//                   });
-//             };
-//       };
-//     });
-//   };
+  if (error_market.textContent !== []){
+    answer_market = error_market.textContent.split(',');
+    answer_market.forEach(element => {
+      for (let i = 1; i < 9; i++){
+        if (element.includes(`market ${i}`)){
+              notyf.error({
+                  message: `Merci de répondre à la question n°${i}`,
+                  dismissible: true
+                  });
+            };
+      };
+    });
+  };
 
-// });
+});
