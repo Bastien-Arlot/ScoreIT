a090c34c2b70b6691b1fc17e8844fb6058280143
guillaume <reygner0224@gmail.com>
2022-03-30 23:13:21 +0200

    test push heroku

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,3 +4,21 @@
 
 //= link bootstrap.min.js
 //= link bootstrap.min.css
+//= link fontawesome.js
+//= link fontawesome.css
+//= link notyf.min.css
+//= link notyf.min.js
+//= link chart.min.js
+
+//= link answer_finances.js
+//= link answer_innovations.js
+//= link answer_markets.js
+//= link answer_offers.js
+//= link answer_strategies.js
+//= link answer_teams.js
+//= link application.js
+//= link cable.js
+//= link flash.js
+//= link startups_show.js
+//= link startups.js
+
