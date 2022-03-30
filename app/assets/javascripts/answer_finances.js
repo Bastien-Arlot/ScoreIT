6e537c37cc937dd07c40b0e4a498e9be8fb5b4d8
guillaume <reygner0224@gmail.com>
2022-03-30 23:18:31 +0200

    test send to heroku

--- app/assets/javascripts/answer_finances.js
+++ app/assets/javascripts/answer_finances.js
@@ -1,47 +1,47 @@
-window.addEventListener("load", function(){
+// window.addEventListener("load", function(){
 
-    const notyf = new Notyf({
-      duration: 6000,
-      position: {
-        x: 'right',
-        y: 'top',
-      }
-    });
+//     const notyf = new Notyf({
+//       duration: 6000,
+//       position: {
+//         x: 'right',
+//         y: 'top',
+//       }
+//     });
 
-    let answer_finance_1 = document.getElementById("answer_finance_1")
+//     let answer_finance_1 = document.getElementById("answer_finance_1")
 
-    answer_finance_1.value = "50";
+//     answer_finance_1.value = "50";
 
-    answer_finance_1.addEventListener("change", function() {
-      if (answer_finance_1.value == "100"){
-        document.getElementById("sizefinance").textContent = "+ 10 0";
-        document.getElementById("p1").textContent = "00€";
-      }else if (answer_finance_1.value == "0"){
-        document.getElementById("sizefinance").textContent = "";
-        document.getElementById("p1").textContent = "Capital social inférieur à 100€";
-      }else{
-        document.getElementById("sizefinance").textContent = answer_finance_1.value;
-        document.getElementById("p1").textContent = "00€";
-      }
-    }, false);
+//     answer_finance_1.addEventListener("change", function() {
+//       if (answer_finance_1.value == "100"){
+//         document.getElementById("sizefinance").textContent = "+ 10 0";
+//         document.getElementById("p1").textContent = "00€";
+//       }else if (answer_finance_1.value == "0"){
+//         document.getElementById("sizefinance").textContent = "";
+//         document.getElementById("p1").textContent = "Capital social inférieur à 100€";
+//       }else{
+//         document.getElementById("sizefinance").textContent = answer_finance_1.value;
+//         document.getElementById("p1").textContent = "00€";
+//       }
+//     }, false);
 
 
-    let error_finance = document.getElementById("error_message_finance");
-    let answer_finance = [];
+//     let error_finance = document.getElementById("error_message_finance");
+//     let answer_finance = [];
 
-    if (error_finance.textContent !== []){
-      answer_finance = error_finance.textContent.split(',');
+//     if (error_finance.textContent !== []){
+//       answer_finance = error_finance.textContent.split(',');
 
-      answer_finance.forEach(element => {
-        for (let i = 1; i < 6; i++){
-          if (element.includes(`finance ${i}`)){
-                notyf.error({
-                    message: `Merci de répondre à la question n°${i}`,
-                    dismissible: true
-                    });
-              };
-        };
-      });
-    };
+//       answer_finance.forEach(element => {
+//         for (let i = 1; i < 6; i++){
+//           if (element.includes(`finance ${i}`)){
+//                 notyf.error({
+//                     message: `Merci de répondre à la question n°${i}`,
+//                     dismissible: true
+//                     });
+//               };
+//         };
+//       });
+//     };
 
-});
+// });
