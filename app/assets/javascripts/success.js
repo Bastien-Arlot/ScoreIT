762c8fd98b028e6a808ac5728ff008cb5032913e
guillaume <reygner0224@gmail.com>
2022-03-31 17:00:12 +0200

    fix assets js

--- app/assets/javascripts/success.js
+++ app/assets/javascripts/success.js
@@ -3,31 +3,35 @@ window.addEventListener("load", function(){
   let countdown = document.getElementById("countdown");
   let redirectDashboard = document.getElementById("redirect_dashboard");
 
-  setTimeout(function(){
-    countdown.textContent = "6";
-  }, 1000);
+  loading();
 
-  setTimeout(function(){
-    countdown.textContent = "5";
-  }, 2000);
-
-  setTimeout(function(){
-    countdown.textContent = "4";
-  }, 3000);
-
-  setTimeout(function(){
-    countdown.textContent = "3";
-  }, 4000);
-
-  setTimeout(function(){
-    countdown.textContent = "2";
-  }, 5000);
-
-  setTimeout(function(){
-    countdown.textContent = "1";
-  }, 6000);
-
-  setTimeout(function(){
-    redirectDashboard.click();
-  }, 7000);
+  function loading(){
+    setTimeout(function(){
+      countdown.textContent = "6";
+    }, 1000);
+  
+    setTimeout(function(){
+      countdown.textContent = "5";
+    }, 2000);
+  
+    setTimeout(function(){
+      countdown.textContent = "4";
+    }, 3000);
+  
+    setTimeout(function(){
+      countdown.textContent = "3";
+    }, 4000);
+  
+    setTimeout(function(){
+      countdown.textContent = "2";
+    }, 5000);
+  
+    setTimeout(function(){
+      countdown.textContent = "1";
+    }, 6000);
+  
+    setTimeout(function(){
+      redirectDashboard.click();
+    }, 7000);
+  }
 });
