6c14ee5f0616aa4e6d73234e07ba6a9131b1e59a
Bastien Arlot <arlot.b@gmail.com>
2022-04-01 11:12:59 +0200

    add controller config routes for pages 404 422 and 500

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -10,7 +9,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
